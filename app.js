5617f7dfdab9cfff0e13606ed9d23e667627635e
madsray <30870145+madsray@users.noreply.github.com>
2018-01-30 10:56:56 -0600

    jquery is working, but testing how a modal looks

---
+++ app.js
@@ -0,0 +1,14 @@
+
+$(() => {
+
+ $('#war').on('click', ()=>{
+   $('<h7>This is War</h7><br>').appendTo('#div');
+   $('<h9>Independent Project</h9><br>').appendTo('#div');
+   $('<h8>Date: November 30, 2017</h8>').appendTo('#div');
+   $('<p>This app was built for project one of General Assemblys Web Development Immersive. HTML, CSS, Javascript, and JQuery were the technologies used to build it. I love card games and this was a popular card game I played as a child, so I chose this game to build for project one. Overall, I am very proud of how it turned out, however, I plan on going back and fixing up so the tie part of the game.</p>').appendTo('#div');
+   $('<a href="https://madsray.github.io/This_Is_War/">| Live Site |</a>').appendTo('#div');
+   $('<a href="https://github.com/madsray/This_Is_War">| GitHub |</a>').appendTo('#div');
+   $('<')
+
+ })
+});
