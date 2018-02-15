a15dae35a1e8a4ba9f00c9a84730355dc1a65a20
madsray <30870145+madsray@users.noreply.github.com>
2018-02-15 16:17:48 -0600

    update nav bar

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+$(()=>{
 $(function() {
 //----- OPEN
 $('[data-popup-open]').on('click', function(e)  {
@@ -12,4 +12,22 @@ var targeted_popup_class = jQuery(this).attr('data-popup-close');
 $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
 e.preventDefault();
 });
+});
+
+// $(window).on('scroll',() =>{
+//   if($("#work").is(":visible")){
+//     console.log("SEAN IS VISIBLE");
+//     $( "#headcont" ).css("background-color","red");
+//     console.log("change");
+// }
+// })
+// $("#about").on('scroll',() =>{
+//   $( "#headcont" ).css("background-color","red");
+//   console.log("change");
+// })
+// $( "#about" ).scroll(function() {
+//   $( "#headcont" ).css("background-color","red");
+//   console.log("change");
+// });
+
 });
